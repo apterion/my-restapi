5b0a6d9869e24c9ac9b1b376498d1840b4608bb6
Sergei Shavlovskiy <apterion@yandex.ru>
2018-07-02 10:11:07 +0300

    created v0.0

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from app import db
 
 class Contracts(db.Model):
     id = db.Column(db.Integer, primary_key = True, unique = True)
-    signed_date = db.Column(db.Date, index = False, unique = False, required=True)
-    text = db.Column(db.Text(), index = False, unique = False, required=True)
+    signed_date = db.Column(db.Date, index = False, unique = False, nullable=False)
+    text = db.Column(db.Text(), index = False, unique = False, nullable=False)
     updated_date = db.Column(db.Date)
 
     def __repr__(self):
